258a975e6037518839aceb5877b1156f5c7b0567
Sameer Khan <s14.sameerkhan@gmail.com>
2024-02-11 18:53:53 +0530

    inital commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/',(req,res)=>{
+    return res.json({
+        message: 'server running fine'
+    })
+})
+
+app.get('/:something',(req,res)=>{
+    return res.json({
+        message: `server running ${req?.params?.something}`
+    })
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log('Server running fine')
+})
